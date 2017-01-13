d966772082978dfa303e82e614a74bcc7d4ec12e
Lyudmil Petrov <lyudmilpetrov79@gmail.com>
2017-01-12 22:51:53 -0500

    Update swStarter.js

--- swStarter.js
+++ swStarter.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/chicagoCTAcash.js', { scope: '/' })
+    navigator.serviceWorker.register('chicagoCTAcash.js', { scope: '/' })
       .then(function (registration) {
           console.log('Service Worker Registered');
       });
